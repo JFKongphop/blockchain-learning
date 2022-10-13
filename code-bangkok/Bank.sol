bb70aa0b21d7094098852809e1e3d4a05133af54
JFKongphop <93483308+JFKongphop@users.noreply.github.com>
2022-10-13 16:40:50 +0700

    bank-contract

--- code-bangkok/Bank.sol
+++ code-bangkok/Bank.sol
@@ -7,17 +7,24 @@ contract Bank{
     mapping (address => uint256) public _balances;
     uint _totalSupply;
 
+    event Deposit(address indexed owner, uint amount);
+    event Withdraw(address indexed owner, uint amount);
+
     function deposit() public payable{
         // _balance += amount;
         _balances[msg.sender] += msg.value;
         _totalSupply += msg.value;
+
+        emit Deposit(msg.sender, msg.value); 
     } 
 
     function withdraw(uint amount) public payable{
         require(amount <= _balances[msg.sender], "Balance is not enough");
+
         payable(msg.sender).transfer(amount);
         _balances[msg.sender] -= amount;
         _totalSupply -= amount;
+        emit Withdraw(msg.sender, amount);
     }
 
     function getBalance() public view returns(uint){
